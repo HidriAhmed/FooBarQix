612d0d35fc7501f103f647c1009a3095460a0a23
Ahmed Hidri <hidri.ahmd@gmail.com>
2016-03-23 12:22:53 +0100

    add class FooBarQixUtils that contains methods for arithmetic operations (ex isMultipleOfThree)) with corresponding test

---
+++ src/main/java/com/ahidri/kata/utils/FooBarQixUtils.java
@@ -0,0 +1,7 @@
+package com.ahidri.kata.utils;
+
+/**
+ * Created by ahidri on 23/03/16.
+ */
+public class FooBarQixUtils {
+}
---
+++ src/test/java/com/ahidri/kata/utils/FooBarQixUtilsTest.java
@@ -0,0 +1,10 @@
+package com.ahidri.kata.utils;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ahidri on 23/03/16.
+ */
+public class FooBarQixUtilsTest {
+
+}
