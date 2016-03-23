414fd36fafe32091ea12556159e491c2309c9626
Ahmed Hidri <hidri.ahmd@gmail.com>
2016-03-23 20:14:41 +0100

    add main class to execute the program

---
+++ src/main/java/com/ahidri/kata/Main.java
@@ -0,0 +1,14 @@
+package com.ahidri.kata;
+
+import com.ahidri.kata.counter.FooBarQixCounter;
+
+/**
+ * Created by ahidri on 23/03/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        FooBarQixCounter fooBarQixCounter = new FooBarQixCounter();
+        fooBarQixCounter.count();
+    }
+}
