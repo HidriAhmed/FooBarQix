00d4d30b18cb27d848c8db8e6807c2aef7087724
Ahmed Hidri <hidri.ahmd@gmail.com>
2016-03-23 12:26:51 +0100

    add test for utils method isMultipleOf3 and correspondings tests

--- src/main/java/com/ahidri/kata/utils/FooBarQixUtils.java
+++ src/main/java/com/ahidri/kata/utils/FooBarQixUtils.java
@@ -4,4 +4,8 @@ package com.ahidri.kata.utils;
  * Created by ahidri on 23/03/16.
  */
 public class FooBarQixUtils {
+
+    public boolean isMultipleOfThree(Integer number) {
+        return number % 3 == 0;
+    }
 }
--- src/test/java/com/ahidri/kata/utils/FooBarQixUtilsTest.java
+++ src/test/java/com/ahidri/kata/utils/FooBarQixUtilsTest.java
@@ -1,5 +1,8 @@
 package com.ahidri.kata.utils;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +10,27 @@ import static org.junit.Assert.*;
  */
 public class FooBarQixUtilsTest {
 
+    private final FooBarQixUtils fooBarQixUtils = new FooBarQixUtils();
+
+    @Test
+    public void should_return_true_when_divisible_by_3(){
+        //given
+
+        //when
+        boolean result =  fooBarQixUtils.isMultipleOfThree(3);
+
+        //then
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    public void should_return_false_when_not_divisible_by_3(){
+        //given
+
+        //when
+        boolean result =  fooBarQixUtils.isMultipleOfThree(1);
+
+        //then
+        Assertions.assertThat(result).isFalse();
+    }
 }
